dabbc7f29dda3210fb5a1ab7cb27f03b5c5e0f38
dlehdgud2380 <dlehdgud2380@gmail.com>
2022-05-25 18:05:27 +0900

    add: import account module

--- module/banking.py
+++ module/banking.py
@@ -1,13 +1,17 @@
+from account import BankUser
+
 class Banking: # Get Account and Enjoy Banking
-    def __init__(self, pin_num: str):
-        pass
-    def search(self):
-        pass
-    def add_account(self):
+    def __init__(self, card_serial: str, pin_num: str):
+        self.user = BankUser(card_serial=card_serial, card_pin=pin_num)
+    
+    def select(self):
         pass
     def balance(self):
         pass
-    def deposit(self):
+    def deposit(self, doller: int):
         pass
-    def withdraw(self):
+    def withdraw(self, doller: int):
         pass
+    
+if __name__ == "__main__":
+    pass
