6452de29e60cb7d1c2785b699c5b2ac08e3a84fc
LeeDongHyeong <dlehdgud2380@gmail.com>
2022-05-25 09:33:01 +0900

    add: divided from account.py

---
+++ module/banking.py
@@ -0,0 +1,13 @@
+class Banking: # Get Account and Enjoy Banking
+    def __init__(self, pin_num: str):
+        pass
+    def search(self):
+        pass
+    def add_account(self):
+        pass
+    def balance(self):
+        pass
+    def deposit(self):
+        pass
+    def withdraw(self):
+        pass
