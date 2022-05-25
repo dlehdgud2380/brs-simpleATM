6fa543aab5ef6f834b8a6bd0a0f92f9112468fd1
dlehdgud2380 <dlehdgud2380@gmail.com>
2022-05-25 23:46:13 +0900

    add: balance, deposit, withdraw

--- module/banking.py
+++ module/banking.py
@@ -3,15 +3,15 @@ from account import BankUser
 class Banking: # Get Account and Enjoy Banking
     def __init__(self, card_serial: str, pin_num: str):
         self.user = BankUser(card_serial=card_serial, card_pin=pin_num)
-    
-    def select(self):
-        pass
     def balance(self):
-        pass
-    def deposit(self, doller: int):
-        pass
-    def withdraw(self, doller: int):
-        pass
+        return self.user.account()
+    def deposit(self, money: int, account_index: int):
+        self.user.balance_update(account_index, money, 'plus')
+    def withdraw(self, money: int, account_index: int):
+        self.user.balance_update(account_index, money, 'minus')
     
 if __name__ == "__main__":
-    pass
+    asd = Banking(card_serial="23600", pin_num="1234")
+    #asd.deposit(10000, 0)
+    #print(asd.balance())
+    #asd.withdraw(60000, 0)
